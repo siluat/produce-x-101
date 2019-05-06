308805c91d51081d426713080b190348ab10b17d
Siluat <siluat@gmail.com>
2019-05-06 18:14:58 +0900

    GA 코드 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize('UA-139579624-1');
+      ReactGA.pageview(window.location.pathname);
+    }
+  }
+
   render() {
     return <p>PRODUCE-X-101</p>
   }
